0c82559c59e12f2c4f87ff2630d42d0722404732
Steve Harris <steve@harrislineage.com>
2022-11-08 09:06:48 -0600

    Update training.js

--- training.js
+++ training.js
@@ -1,12 +1,39 @@
 jQuery(document).ready(function ($) {
     if ($('#q1fundamentals')) {
-        $.getJSON('https://raw.githubusercontent.com/qsicst/training/main/training.json', function (data) {
-            var year = new Date().getFullYear();
-            var month = new Date().getMonth() + 1;
-            var day = new Date().getDate();
-
-            $('#q1fundamentals').append('Hello!');
+        $.getJSON('https://raw.githubusercontent.com/qsicst/training/main/q1fund.json', function (data) {
+            var now = new Date();
+            var html = '<ul>';
+            for (var i = 0; i < data.Q1Fundamentals.length; i++) {
+                var trainingDate = new Date(
+                    data.Q1Fundamentals[i].Year,
+                    data.Q1Fundamentals[i].Month - 1,
+                    data.Q1Fundamentals[i].StartDay
+                );
+                if (monthDiff(now, trainingDate) <= 6) {
+                    var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+                    html += `<li>
+                        ${months[trainingDate.getMonth()]} 
+                        ${data.Q1Fundamentals[i].StartDay}-${data.Q1Fundamentals[i].StartDay}, 
+                        ${data.Q1Fundamentals[i].Year}
+                        </li>
+                    `;
+                }
+            }
+            html += '</ul>';
+            $('#q1fundamentals').append(html);
         })
+    }
+
+    function monthDiff(d1, d2) {
+        var months;
+        months = (d2.getFullYear() - d1.getFullYear()) * 12;
+        months -= d1.getMonth();
+        months += d2.getMonth();
+        return months <= 0 ? 0 : months;
+    }
+
+    function test(d1, d2) {
+        return monthDiff(d1, d2);
     }
 });
 
