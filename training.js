a273ae8083c52ed6f36754f14f92ea26e2f9cee1
Steve Harris <steve@harrislineage.com>
2022-11-08 09:24:33 -0600

    Add additional training setup

--- training.js
+++ training.js
@@ -1,20 +1,21 @@
 jQuery(document).ready(function ($) {
+    // Q1 Fundamentals Training
     if ($('#q1fundamentals')) {
-        $.getJSON('https://raw.githubusercontent.com/qsicst/training/main/q1fund.json', function (data) {
+        $.getJSON('https://raw.githubusercontent.com/qsicst/training/main/q1.json', function (data) {
             var now = new Date();
             var html = '<ul>';
-            for (var i = 0; i < data.Q1Fundamentals.length; i++) {
+            for (var i = 0; i < data.training.length; i++) {
                 var trainingDate = new Date(
-                    data.Q1Fundamentals[i].Year,
-                    data.Q1Fundamentals[i].Month - 1,
-                    data.Q1Fundamentals[i].StartDay
+                    data.training[i].Year,
+                    data.training[i].Month - 1,
+                    data.training[i].StartDay
                 );
                 if (monthDiff(now, trainingDate) <= 6) {
                     var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
                     html += `<li>
                         ${months[trainingDate.getMonth()]} 
-                        ${data.Q1Fundamentals[i].StartDay}-${data.Q1Fundamentals[i].StartDay}, 
-                        ${data.Q1Fundamentals[i].Year}
+                        ${data.training[i].StartDay}-${data.training[i].EndDay}, 
+                        ${data.training[i].Year}
                         </li>
                     `;
                 }
@@ -23,6 +24,107 @@ jQuery(document).ready(function ($) {
             $('#q1fundamentals').append(html);
         })
     }
+    // Q1 Practitioner Training
+    if ($('#q1practitioner')) {
+        $.getJSON('https://raw.githubusercontent.com/qsicst/training/main/q1.json', function (data) {
+            var now = new Date();
+            var html = '<ul>';
+            for (var i = 0; i < data.training.length; i++) {
+                var trainingDate = new Date(
+                    data.training[i].Year,
+                    data.training[i].Month - 1,
+                    data.training[i].StartDay
+                );
+                if (monthDiff(now, trainingDate) <= 6) {
+                    var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+                    html += `<li>
+                        ${months[trainingDate.getMonth()]} 
+                        ${data.training[i].StartDay}-${data.training[i].EndDay + 1}, 
+                        ${data.training[i].Year}
+                        </li>
+                    `;
+                }
+            }
+            html += '</ul>';
+            $('#q1practitioner').append(html);
+        })
+    }
+    // Q2 Fundamentals Training
+    if ($('#q2fundamentals')) {
+        $.getJSON('https://raw.githubusercontent.com/qsicst/training/main/q2.json', function (data) {
+            var now = new Date();
+            var html = '<ul>';
+            for (var i = 0; i < data.training.length; i++) {
+                var trainingDate = new Date(
+                    data.training[i].Year,
+                    data.training[i].Month - 1,
+                    data.training[i].StartDay
+                );
+                if (monthDiff(now, trainingDate) <= 6) {
+                    var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+                    html += `<li>
+                        ${months[trainingDate.getMonth()]} 
+                        ${data.training[i].StartDay}-${data.training[i].EndDay}, 
+                        ${data.training[i].Year}
+                        </li>
+                    `;
+                }
+            }
+            html += '</ul>';
+            $('#q1fundamentals').append(html);
+        })
+    }
+    // Q2 Practitioner Training
+    if ($('#q2practitioner')) {
+        $.getJSON('https://raw.githubusercontent.com/qsicst/training/main/q2.json', function (data) {
+            var now = new Date();
+            var html = '<ul>';
+            for (var i = 0; i < data.training.length; i++) {
+                var trainingDate = new Date(
+                    data.training[i].Year,
+                    data.training[i].Month - 1,
+                    data.training[i].StartDay
+                );
+                if (monthDiff(now, trainingDate) <= 6) {
+                    var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+                    html += `<li>
+                        ${months[trainingDate.getMonth()]} 
+                        ${data.training[i].StartDay}-${data.training[i].EndDay + 1}, 
+                        ${data.training[i].Year}
+                        </li>
+                    `;
+                }
+            }
+            html += '</ul>';
+            $('#q2practitioner').append(html);
+        })
+    }
+    // Lead Auditor Training
+    if ($('#leadauditor')) {
+        $.getJSON('https://raw.githubusercontent.com/qsicst/training/main/leadaudit.json', function (data) {
+            var now = new Date();
+            var html = '<ul>';
+            for (var i = 0; i < data.training.length; i++) {
+                var trainingDate = new Date(
+                    data.training[i].Year,
+                    data.training[i].Month - 1,
+                    data.training[i].StartDay
+                );
+                if (monthDiff(now, trainingDate) <= 6) {
+                    var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+                    html += `<li>
+                        ${months[trainingDate.getMonth()]} 
+                        ${data.training[i].StartDay}-${data.training[i].EndDay}, 
+                        ${data.training[i].Year}
+                        </li>
+                    `;
+                }
+            }
+            html += '</ul>';
+            $('#leadauditor').append(html);
+        })
+    }
+
 
     function monthDiff(d1, d2) {
         var months;
@@ -36,4 +138,3 @@ jQuery(document).ready(function ($) {
         return monthDiff(d1, d2);
     }
 });
-
